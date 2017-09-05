be2957109362fd56cc71eba5fdb1879cd39df06f
Ellery Zhang <allotory@msn.com>
2017-09-05 22:18:17 +0800

    feat: update db

--- src/main/resources/db/coeligena.sql
+++ src/main/resources/db/coeligena.sql
@@ -24,13 +24,16 @@ CREATE TABLE IF NOT EXISTS auto_user (
 
 /*
  * 角色表
+ * 一定数量的权限的集合，权限的载体
  */
 CREATE TABLE IF NOT EXISTS role (
-    id       INT UNSIGNED        NOT NULL AUTO_INCREMENT, /* 角色 ID（唯一标识） */
-    rolename VARCHAR(32)         NOT NULL, /* 角色名 */
-    pid      INT UNSIGNED                 DEFAULT '0', /* 父角色 ID */
-    status   TINYINT(1) UNSIGNED NOT NULL, /* 启用状态：0-表示禁用，1-表示启用 */
-    remark   VARCHAR(256)                 DEFAULT NULL, /* 备注信息 */
+    id          INT UNSIGNED        NOT NULL AUTO_INCREMENT, /* 角色 ID（唯一标识） */
+    rolename    VARCHAR(32)         NOT NULL, /* 角色名 */
+    pid         INT UNSIGNED                 DEFAULT '0', /* 父角色 ID */
+    status      TINYINT(1) UNSIGNED NOT NULL, /* 启用状态：0-表示禁用，1-表示启用 */
+    create_time TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP, /* 创建时间（时间戳） */
+    update_time TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, /* 更改时间（时间戳）*/
+    remark      VARCHAR(256)                 DEFAULT NULL, /* 备注信息 */
     PRIMARY KEY (id)
 );
 
@@ -47,14 +50,16 @@ CREATE TABLE IF NOT EXISTS role_auth_user (
  * 权限表
  */
 CREATE TABLE IF NOT EXISTS permission (
-    id     INT UNSIGNED         NOT NULL AUTO_INCREMENT, /* 权限 ID（唯一标识） */
-    name   VARCHAR(32)          NOT NULL, /* 权限名称（英文名，对应应用控制器、应用、方法名） */
-    title  VARCHAR(64)                   DEFAULT NULL, /* 权限名称 */
-    status TINYINT(1)                    DEFAULT '0', /* 启用状态：0-表示禁用，1-表示启用 */
-    remark VARCHAR(256)                  DEFAULT NULL, /* 备注信息 */
-    sort   SMALLINT(6) UNSIGNED          DEFAULT '50', /* 排序值（默认值为50） */
-    pid    SMALLINT(6) UNSIGNED NOT NULL, /* 父权限 ID（如:方法pid对应相应的控制器） */
-    level  TINYINT(1) UNSIGNED  NOT NULL, /* 权限类型：1-表示应用（模块）, 2-表示控制器, 3-表示方法 */
+    id          INT UNSIGNED         NOT NULL AUTO_INCREMENT, /* 权限 ID（唯一标识） */
+    name        VARCHAR(32)          NOT NULL, /* 权限名称（英文名，对应应用控制器、应用、方法名） */
+    title       VARCHAR(64)                   DEFAULT NULL, /* 权限名称 */
+    status      TINYINT(1)                    DEFAULT '0', /* 启用状态：0-表示禁用，1-表示启用 */
+    remark      VARCHAR(256)                  DEFAULT NULL, /* 备注信息 */
+    sort        SMALLINT(6) UNSIGNED          DEFAULT '50', /* 排序值（默认值为50） */
+    pid         SMALLINT(6) UNSIGNED NOT NULL, /* 父权限 ID（如:方法pid对应相应的控制器） */
+    level       TINYINT(1) UNSIGNED  NOT NULL, /* 权限类型：1-表示应用（模块）, 2-表示控制器, 3-表示方法 */
+    create_time TIMESTAMP            NOT NULL DEFAULT CURRENT_TIMESTAMP, /* 创建时间（时间戳） */
+    update_time TIMESTAMP            NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, /* 更改时间（时间戳）*/
     PRIMARY KEY (id)
 );
 
