33fc8eaf2133d311a9fe609e19467c2e4b250d78
Ellery Zhang <allotory@msn.com>
2018-06-28 23:02:13 +0800

    feat: test scroll

--- src/main/webapp/resources/js/answer_collapse.js
+++ src/main/webapp/resources/js/answer_collapse.js
@@ -14,7 +14,16 @@ function tuckUpAnswer(id) {
     $('#ans-unfold-' + id).removeClass('hide');
     $('#tuck-up-' + id).addClass('hide');
 
-    window.location.hash = '#ans-inner-' + id;
+    alert($(window).height()); //浏览器当前窗口可视区域高度
+    alert($(document).height()); //浏览器当前窗口文档的高度
+    alert($(document.body).height());//浏览器当前窗口文档body的高度
+    alert($(document.body).outerHeight(true));//浏览器当前窗口文档body的总高度 包括border padding margin
+    alert($(window).width()); //浏览器当前窗口可视区域宽度
+    alert($(document).width());//浏览器当前窗口文档对象宽度
+    alert($(document.body).width());//浏览器当前窗口文档body的高度
+    alert($(document.body).outerWidth(true));//浏览器当前窗口文档body的总宽度 包括border padding margin
+    var fold = $(window).height() + $(window).scrollTop();
+    console.log(fold);
 }
 
 // 展开全部回答
