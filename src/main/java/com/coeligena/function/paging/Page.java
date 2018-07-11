d4f19190c2d44e523ee6a2f66096d13d2fc65fe0
Ellery Zhang <allotory@msn.com>
2018-07-11 22:38:25 +0800

    feat: calc page data

--- src/main/java/com/coeligena/function/paging/Page.java
+++ src/main/java/com/coeligena/function/paging/Page.java
@@ -10,7 +10,10 @@ public class Page {
     private int pageNum;
     // 每页的数量
     private int pageSize;
-    // 当前页的数量
+
+    // 总页数
+    private int pages;
+    // 总记录数
     private int size;
 
     // 可以在页面中"显示startRow到endRow 共size条数据"
@@ -18,9 +21,6 @@ public class Page {
     private int startRow;
     // 当前页面最后一个元素在数据库中的行号
     private int endRow;
-    
-    // 总页数
-    private int pages;
 
     // 前一页
     private int prePage;
@@ -35,6 +35,7 @@ public class Page {
     private boolean hasPreviousPage = false;
     // 是否有下一页
     private boolean hasNextPage = false;
+
     // 导航页码数
     private int navigatePages;
     // 所有导航页号
@@ -45,6 +46,94 @@ public class Page {
     private int navigateLastPage;
 
     public Page() {
+    }
+
+    public Page(int pageNum, int pageSize) {
+        this.pageNum = pageNum;
+        this.pageSize = pageSize;
+
+        calcPages();
+        calcPreNextPage();
+        calcPageBoundary();
+
+        this.startRow = (this.pageNum - 1) * this.pageSize;
+    }
+
+    /**
+     * 计算总页数
+     */
+    private void calcPages() {
+        // 总页数
+        if (this.size < 0) {
+            this.pages = 1;
+        } else {
+            if (this.pageSize > 0) {
+                this.pages = this.size % this.pageSize == 0?
+                        this.size / this.pageSize : this.size / this.pageSize + 1;
+            } else {
+                this.pages = 0;
+            }
+        }
+    }
+
+    /**
+     * 计算前后页
+     */
+    private void calcPreNextPage() {
+        if (this.pageNum > 1) {
+            this.prePage = this.pageNum - 1;
+        }
+        if (this.pageNum < this.pages) {
+            this.nextPage = this.pageNum + 1;
+        }
+    }
+
+    /**
+     * 计算页边界
+     */
+    private void calcPageBoundary() {
+        this.isFirstPage = (this.pageNum == 1);
+        this.isLastPage = (this.pageNum == this.pages) || (this.pages == 0);
+        this.hasPreviousPage = (this.pageNum > 1);
+        this.hasNextPage = (this.pageNum < this.pages);
+    }
+
+    /**
+     * 计算导航页码
+     */
+    private void calcNavigatePageNums() {
+
+        if (this.pages <= this.navigatePages) {
+            //当总页数小于或等于导航页码数时
+            this.navigatePageNums = new int[this.pages];
+            for (int i = 0; i < this.pages; i++) {
+                this.navigatePageNums[i] = i + 1;
+            }
+        } else {
+            //当总页数大于导航页码数时
+            this.navigatePageNums = new int[this.navigatePages];
+            int startNum = this.pageNum - this.navigatePages / 2;
+            int endNum = this.pageNum + this.navigatePages / 2;
+
+            if (startNum < 1) {
+                startNum = 1;
+                //(最前navigatePages页
+                for (int i = 0; i < navigatePages; i++) {
+                    navigatePageNums[i] = startNum++;
+                }
+            } else if (endNum > pages) {
+                endNum = pages;
+                //最后navigatePages页
+                for (int i = navigatePages - 1; i >= 0; i--) {
+                    navigatePageNums[i] = endNum--;
+                }
+            } else {
+                //所有中间页
+                for (int i = 0; i < navigatePages; i++) {
+                    navigatePageNums[i] = startNum++;
+                }
+            }
+        }
     }
 
     public int getPageNum() {
@@ -67,8 +156,20 @@ public class Page {
         return size;
     }
 
+    /**
+     * 设置总的记录数
+     * @param size 总的记录数
+     */
     public void setSize(int size) {
         this.size = size;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
     }
 
     public int getStartRow() {
@@ -85,14 +186,6 @@ public class Page {
 
     public void setEndRow(int endRow) {
         this.endRow = endRow;
-    }
-
-    public int getPages() {
-        return pages;
-    }
-
-    public void setPages(int pages) {
-        this.pages = pages;
     }
 
     public int getPrePage() {
