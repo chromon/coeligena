7c78df7024f4724e9798707037932ae6e5abec24
chromon <allotory@msn.com>
2021-05-17 20:53:22 +0800

    feat: update version

--- src/main/resources/db/roles_data.sql
+++ src/main/resources/db/roles_data.sql
@@ -1,8 +1,8 @@
 
-INSERT INTO `roles` (`id`, `rolename`, `role_content`, `pid`, `status`, `create_time`, `update_time`, `remark`) VALUES
-(0, 'Administrators', '超级管理员', 0, 1, null, null, ''),
-(0, 'ContentReviewAdministrators', '内容审核管理员', 1,  1, null, null, ''),
-(0, 'UnauthenticatedUser', '未验证用户', 0, 1, null, null, ''),
-(0, 'RegularUser', '普通用户', 0, 1, null, null, ''),
-(0, 'BannedUser', '封禁用户', 0, 1, null, null, ''),
-(0, 'CorporateUser', '企业会员', 0, 1, null, null, '');
+INSERT INTO `roles` (`id`, `rolename`, `role_content`, `pid`, `status`, `remark`) VALUES
+(0, 'Administrators', '超级管理员', 0, 1, ''),
+(0, 'ContentReviewAdministrators', '内容审核管理员', 1,  1, ''),
+(0, 'UnauthenticatedUser', '未验证用户', 0, 1, ''),
+(0, 'RegularUser', '普通用户', 0, 1, ''),
+(0, 'BannedUser', '封禁用户', 0, 1, ''),
+(0, 'CorporateUser', '企业会员', 0, 1, '');
